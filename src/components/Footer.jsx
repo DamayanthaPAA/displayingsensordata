95db78dc3e5943360d7e52befd32f7c0a0161fff
Anura Damayantha <161550103+DamayanthaPAA@users.noreply.github.com>
2025-04-03 16:00:29 +0300

    add update

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="bg-green-900 text-white text-center py-6 mt-8">
-        <p>&copy; {new Date().getFullYear()} mqttLink. All rights reserved.</p>
-      </footer>
-    );
-  }
